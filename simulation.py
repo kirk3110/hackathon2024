7072bc3a3efc9a4a0295d4b41c3abb3ace7eb508
kirk3110 <saito.kiriko@gmail.com>
2024-05-21 20:41:01 +0900

    初速度を角度と速度から算出するようにした

--- simulation.py
+++ simulation.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-def run_simulation(mass1, mass2, pos1, pos2, vel1, vel2, radius1, radius2, simulation_time, time_step, decay):
+def run_simulation(mass1, mass2, pos1, pos2, angle1, angle2, speed1, speed2, radius1, radius2, simulation_time, time_step, decay):
     # シミュレーションの実行
     times = np.arange(0, simulation_time, time_step)
     positions1 = []
     positions2 = []
+
+    vel1 = calculate_velocity(angle1, speed1)
+    vel2 = calculate_velocity(angle2, speed2)
 
     for t in times:
         positions1.append(pos1.copy())
@@ -28,3 +31,12 @@ def run_simulation(mass1, mass2, pos1, pos2, vel1, vel2, radius1, radius2, simul
             vel2 = v2 - 2 * mass1 / (mass1 + mass2) * np.dot(v2 - v1, pos2 - pos1) / np.linalg.norm(pos2 - pos1)**2 * (pos2 - pos1)
 
     return positions1, positions2
+
+
+def calculate_velocity(angle, speed):
+    # 角度をラジアンに変換
+    angle_rad = np.radians(angle)
+    # 速度ベクトルを計算
+    vx = speed * np.cos(angle_rad)
+    vy = speed * np.sin(angle_rad)
+    return np.array([vx, vy])
